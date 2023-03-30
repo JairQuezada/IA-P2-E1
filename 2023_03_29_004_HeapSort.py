972a252db233f17e8be1f4e3585d1096aa67f8be
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-03-29 23:13:09 -0600

    Metodo Interno 4

---
+++ 2023_03_29_004_HeapSort.py
@@ -0,0 +1,35 @@
+import random 
+lista = [random.randint(0,150)for _ in range(15)]
+
+#Se crea un lista de 15 datos, los cuales gracias al random se sacan listas diferentes cada vez que se ejecuta el programa
+
+print("Esta es la lista que se va a ordenar:   ",lista)
+#BubbleSort usa la lista que creamos previamente para acomodarlas
+
+def heap_sort(lista):
+    def heapify(lista, n, i):
+        largo = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < n and lista[i] < lista[l]:
+            largo = l
+        if r < n and lista[largo] < lista[r]:
+            largo = r
+        if largo != i:
+            lista[i], lista[largo] = lista[largo], lista[i]
+            heapify(lista, n, largo)
+
+    n = len(lista)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(lista, n, i)
+
+    for i in range(n-1, 0, -1):
+        lista[0], lista[i] = lista[i], lista[0]
+        heapify(lista, i, 0)
+
+    return lista
+
+lista_nueva = heap_sort(lista)
+print("Esta es la lista ya ordenada: \t\t", lista_nueva)
+#Se imprime la lista completa ya acomodada
